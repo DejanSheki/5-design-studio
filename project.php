681f1ed161b959ef10f1261ab0b86e76a7b8b9b7
Dejan <dejan.sheki.lukic@gmail.com>
2020-10-08 16:48:53 +0200

    Slider and responsive

--- project.php
+++ project.php
@@ -1,5 +1,7 @@
 <?php include 'single-headers.php' ?>
 
+<!--projects-->
+
 <section class="projects container">
     <article>
         <h2 class="headings">Projects</h2>
@@ -8,6 +10,43 @@
     <article>
         <img src="img/projects.jpg" alt="">
     </article>
+</section>
+
+<!--slider--> 
+
+<section class="slider">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="img/works1.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works2.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works3.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works4.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works5.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works6.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works7.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works8.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works9.jpg" alt="">
+        </div>
+        <div class="item">
+            <img src="img/works10.jpg" alt="">
+        </div>
+    </div>
 </section>
 
 <?php include 'footer.php' ?>
