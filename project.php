8bb1cd707921ed40502321f1820b01ca711a450b
Dejan <dejan.sheki.lukic@gmail.com>
2020-10-08 15:51:51 +0200

    Projects

--- project.php
+++ project.php
@@ -1,4 +1,13 @@
 <?php include 'single-headers.php' ?>
 
+<section class="projects container">
+    <article>
+        <h2 class="headings">Projects</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quos delectus doloribus sapiente commodi repellat iure, nemo id nam et nulla tempore porro ea dolores eveniet tempora illo reiciendis ad! Nulla reiciendis recusandae assumenda odit adipisci, nobis eveniet tenetur quam aliquam perferendis repellat aut dolorum necessitatibus in praesentium deserunt ipsum.</p>
+    </article>
+    <article>
+        <img src="img/projects.jpg" alt="">
+    </article>
+</section>
 
 <?php include 'footer.php' ?>
